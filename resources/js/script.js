924ae9e34345243f314ad4cf1f7db568dd1ca111
Amarachi Aka <124574651+Grace-Aka@users.noreply.github.com>
2023-06-29 15:55:42 +0100

    fix errors in script.js

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,30 +1,17 @@
 const prompt = require('prompt-sync')();
 // Prompt the user to enter a number
-let input = prompt('Please enter a number: ');
-
+const input = prompt('Please enter a number: ');
 // Parse the user's input to an integer
 let number = parseInt(input);
-
 // Loop through numbers from 1 to the user's input
-function fizzBuzz() {
-  for (var i = 1; i <= number; i++) {
-    // Check if the number is divisible by both 3 and 5
-    if (i % 3 == 0 && i % 5 == 0) {
-      console.log("FizzBuzz");
-    }
-  // Check if the number is divisible by 3
-    else if (i % 3 == 0) {
-      console.log("Fizz");
-    }
-  // Check if the number is divisible by 5
-    else if (i % 5 == 0) {
-      console.log("Buzz");
-    }
-  // For other numbers, simply print the number
-    else {
-      console.log(i);
-    }
+for (let i = 1; i <= number; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log('FizzBuzz');
+  } else if (i % 3 === 0) {
+    console.log('Fizz');
+  } else if (i % 5 === 0) {
+    console.log('Buzz');
+  } else {
+    console.log(i);
   }
-
 }
-fizzBuzz();
